91e1856b2f714529796ef797c7bda2f54cbba4d3
Mac <mac@MacBook-Air.local>
2024-06-04 21:06:20 +0300

    Commit Admin

--- src/core/settings.py
+++ src/core/settings.py
@@ -124,10 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-    'static/admin/',
-)
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
