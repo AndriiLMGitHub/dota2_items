5e8070c711510fcf63cc61dfdf37e8f1334591e1
Mac <mac@MacBook-Air.local>
2024-06-04 21:10:48 +0300

    Commit admin

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Dota2Item, Dota2ItemImage
 
-# Register your models here.
-admin.site.register(Dota2Item)
+
+class Dota2ItemAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'market_hash_name', 'class_name')
+    search_fields = ('market_hash_name', )
+
+
+admin.site.register(Dota2Item, Dota2ItemAdmin)
 admin.site.register(Dota2ItemImage)
