24db47bae457d81eb1fa356108271db90bc1a74d
Mac <mac@MacBook-Air.local>
2024-07-03 08:42:08 +0300

    Change url.py in app

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<str:pk>', views.detail_item, name="detail_item"),
+    path('<int:pk>', views.detail_item, name="detail_item"),
     path('search/', views.search_view, name='search'),
 ]
